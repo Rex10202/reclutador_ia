9f27a629696d4579289226baca19df1e2bd11c7b
luisariass <ariasl@utb.edu.co>
2025-12-02 11:37:02 -0500

    archivos de configuracion para configurar el frontend

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\luisarias\Documents\reclutador_ia\frontend\postcss.config.js
+module.exports = {
+  plugins: {
+    'postcss-nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
